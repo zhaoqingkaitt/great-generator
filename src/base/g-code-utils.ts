1c8f0bb41130410c72032fcccfdc258f63b9ed0b
zhaoqingkaitt@163.com <zhaoqingkaitt@163.com>
2021-01-29 18:30:14 +0800

    生成一些组织机构

--- src/base/g-code-utils.ts
+++ src/base/g-code-utils.ts
@@ -3,6 +3,6 @@ import {DataGeneratorUtils} from "great-jsutils";
 export class GCodeUtils {
 
     static letter({number = 1, upperCase = true, length = 1, prefix = '', suffix=''} = {}): Array<string> | string {
-        return DataGeneratorUtils.letter({size: number, upperCase , length, prefix, suffix})
+        return DataGeneratorUtils.letter({size: number, upperCase , length, prefix, suffix});
     }
 }
--- src/org/g-organization-utils.ts
+++ src/org/g-organization-utils.ts
@@ -1,10 +1,28 @@
 import {DataGeneratorUtils} from "great-jsutils";
+import {UserModel} from "../base/model/user-model";
+import {GContactUtils} from "../base/g-contact-utils";
+import {GOrganizationModel} from "./model/g-organization-model";
 
 export class GOrganizationUtils {
 
     static orgName(){
         const orgNameList = ['山东**总公司', '北京**总公司','河北**总公司', '内蒙古****总公司', '山东济南**总公司'];
         return DataGeneratorUtils.getElementByArray(orgNameList);
+    }
+
+    static org({number = 1}){
+        number = number>=1?number:1;
+        const orgList:Array<UserModel> = [];
+        for (let i=0;i<number;i++){
+            const org = new GOrganizationModel();
+            org.name = DataGeneratorUtils.personName();
+            org.orgCode = DataGeneratorUtils.letter({length: 10});
+            org.phone = DataGeneratorUtils.mobile();
+            org.address = GContactUtils.address();
+            org.orgName = GOrganizationUtils.orgName();
+            orgList.push(org);
+        }
+        return number==1?orgList[0]: orgList;
     }
 
 }
