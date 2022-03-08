d67997861e8fddb322d334a186e34a8171c43786
zhaoqingkaitt@163.com <zhaoqingkaitt@163.com>
2022-03-08 08:51:05 +0800

    升级工具版本

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,16 @@ import {GCodeUtils} from "./base/g-code-utils";
 import {GColorUtils} from "./base/g-color-utils";
 import {GOrganizationUtils} from "./org/g-organization-utils";
 import {GDepartmentUtils} from "./org/g-department-utils";
+import {GTreeUtils} from "./base/g-tree-utils";
 
 export default {
-    GNumberUtils,
-    GColorUtils,
-    GTimeUtils,
-    GContactUtils,
-    GPersonUtils,
     GCodeUtils,
+    GColorUtils,
+    GContactUtils,
+    GNumberUtils,
+    GPersonUtils,
+    GTimeUtils,
+    GTreeUtils,
     GOrganizationUtils,
     GDepartmentUtils
 }
