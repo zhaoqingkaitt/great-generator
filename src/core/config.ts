b7b6059e133f6bac1e087f3ccbcc521fe97a7c09
zhaoqingkaitt@163.com <zhaoqingkaitt@163.com>
2022-04-06 14:39:03 +0800

    公共配置数据

---
+++ src/core/config.ts
@@ -0,0 +1,20 @@
+/**
+ * 公共配置数据,
+ * 二次开发时，方便从自定义数据中，生成随机数据
+ */
+export class GConfigData{
+
+    private  static config;
+
+    constructor(
+        private pseronNames?:Array<String>,
+        private dateTimes?:Array<String>,
+        private colors?:Array<String>,
+    ) {
+    }
+
+    static init(config:GConfigData =  new GConfigData()){
+        this.config = config;
+    }
+
+}
