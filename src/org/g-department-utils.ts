ec6fa17094126bef2d2c1267c3a9570f771ef753
Hello <zhaoqingkaitt@163.com>
2023-04-11 10:43:41 +0800

    Update g-department-utils.ts
    部门名称随机完善

--- src/org/g-department-utils.ts
+++ src/org/g-department-utils.ts
@@ -4,8 +4,7 @@ import {GDepartmentModel} from "./model/g-department-model";
 
 export class GDepartmentUtils {
 
-    static deptName(){
-        const deptNameList = ['开发部', '测试部', 'UI部', '研发管理部', '产品部', '办公室', '市场部', '商务部'];
+    static deptName(deptNameList =  ['开发部', '测试部', 'UI部', '研发管理部', '产品部', '办公室', '市场部', '商务部']){
         return DataGeneratorUtils.getElementByArray(deptNameList);
     }
 
