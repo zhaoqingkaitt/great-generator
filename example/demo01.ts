1970f9a0523370b17774f3394691c80808c728f4
Hello <zhaoqingkaitt@163.com>
2024-04-18 10:27:35 +0800

    Update demo01.ts
    111

--- example/demo01.ts
+++ example/demo01.ts
@@ -13,5 +13,6 @@ console.log('generator：', GNumberUtils.randomFloat({number: 10}));
 console.log('GTimeUtils：', GTimeUtils.str({number:3}));
 */
 const nodes = GTreeUtils.tree({maxLevel: 3, maxNum: 7});
+console.log(nodes);
 console.log(JSON.stringify(nodes));
 console.log(NumberUtils.randomInt({max:5, number: 20}));
